71c121f02498bfeb232079f3ca51d971be02c114
India Heath <ih047@hdm-stuttgart.de>
2024-05-01 22:29:28 +0200

    added global css variables to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,31 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [  "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-],
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+module.exports = withMT({
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#171a2c',
+        secondary: ' #8091be',
+        tertiary: '#563d4f',
+
+        text: '#ffffff',
+        'text-dark': '#dddddd',
+        'text-darker': '#bbbbbb',
+        'text-inv': '#000000',
+        'text-inv-light': '#1a1a1a',
+
+        success: '#2bb32b',
+        'success-light': '#a5f3a5',
+
+        error: ' #e2665b',
+        'error-light': '#db7d74',
+      },
+      fontFamily: {
+        main: 'Montserrat',
+        display: ['Kumar One', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-};
+});
