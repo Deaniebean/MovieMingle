04064a32aa620b0731f411b9cd001ac7dc57fa52
India Heath <ih047@hdm-stuttgart.de>
2024-06-01 16:36:47 +0200

    props added for centering option

--- frontend/src/components/innerComponents/FilterSeparator.tsx
+++ frontend/src/components/innerComponents/FilterSeparator.tsx
@@ -3,18 +3,20 @@ import React from 'react';
 // Define the props interface
 interface FilterSeparatorProps {
   text: string;
+  justify?: string;
 }
 
 // Define default props
 const defaultProps: Partial<FilterSeparatorProps> = {
   text: 'default value for prop1',
+  justify: ''
 };
 
 // Define the component
 const FilterSeparator: React.FC<FilterSeparatorProps> = (props) => {
   // You can use props.prop1 and props.prop2 here
   return (
-    <div className="flex items-center gap-3 py-3.5">
+    <div className={props.justify + "flex items-center gap-3 py-3.5"}>
       <h2 className="text-light text-nowrap text-xl">{props.text}</h2>
       <hr className="my-2 border-light w-full" />
     </div>
