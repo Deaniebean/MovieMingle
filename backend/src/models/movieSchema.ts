993cf8a495cd038e8cfe67b85a59989f357bd608
Nadine Eunous <nadineeunous@Nadines-MacBook-Air.local>
2024-06-11 11:13:44 +0200

    fix reset-password and geet watchlist

--- backend/src/models/movieSchema.ts
+++ backend/src/models/movieSchema.ts
@@ -1,5 +1,7 @@
 // This model is created to simplify testing my functions in isiolation from the database
 interface MovieModel{
+    _id: string,
+    id: string,
     original_title: string,
     overview: string,
     genre: string[], // convert id numbers to strings before adding to model
