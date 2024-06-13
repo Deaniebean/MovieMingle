986aaaebf7bd321f9fa27506cd1e8e69752cfc27
Nadine Eunous <nadineeunous@Nadines-MacBook-Air.local>
2024-06-13 12:02:39 +0200

    still not working

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 const connectDB = async () => {
   try {
-    const url = "mongodb://localhost:27017/moviemingle";
+    const url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@db:27017/moviemingle?authSource=admin`;
     await mongoose.connect(url);
     console.log('connected to database moviemingle ...');
   } catch (error) {
