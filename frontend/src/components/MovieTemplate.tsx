4397d9b2892060deb9f33950fe1d40b8a5468bcf
af110 <af110@hdm-stuttgart.de>
2024-06-13 12:49:57 +0200

    added the trailer link

--- frontend/src/components/MovieTemplate.tsx
+++ frontend/src/components/MovieTemplate.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './MovieTemplate.css';
 import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
 interface MovieProps {
   movie: {
@@ -9,10 +10,34 @@ interface MovieProps {
     original_title: string;
     poster_path: string;
     release_date: string;
+    trailer?: string; // Optional trailer URL
   };
 }
 
 const MovieTemplate: React.FC<MovieProps> = ({ movie }) => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+  const openTrailer = () => {
+    if (movie.trailer) {
+      setModalOpen(true);
+    } else {
+      console.error('No trailer URL available');
+    }
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const getYouTubeEmbedUrl = (url: string) => {
+    const videoId = url.split('v=')[1];
+    const ampersandPosition = videoId.indexOf('&');
+    if (ampersandPosition !== -1) {
+      return `https://www.youtube.com/embed/${videoId.substring(0, ampersandPosition)}`;
+    }
+    return `https://www.youtube.com/embed/${videoId}`;
+  };
+
   return (
     <div className="movie">
       <h1 className="TitleOfMovie">{movie.original_title}</h1>
@@ -23,17 +48,35 @@ const MovieTemplate: React.FC<MovieProps> = ({ movie }) => {
         alt="Movie Poster"
       />
       <p className="dateAdded">Added on: {new Date(movie.release_date).toLocaleDateString()}</p>
-      <button className="buttonTrailer">
+      <button className="buttonTrailer" onClick={openTrailer}>
         <PlayArrowRoundedIcon />
         Trailer
       </button>
       <Link to={`/movie/${movie._id}`} className="buttonDetails">
         More details
       </Link>
+      {modalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <button className="close-button" onClick={closeModal}>
+              <CloseRoundedIcon />
+            </button>
+            <iframe
+              width="560"
+              height="315"
+              src={getYouTubeEmbedUrl(movie.trailer || '')}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 export default MovieTemplate;
+
 
 
