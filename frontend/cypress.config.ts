43ac20db71fa3248a4d1d453ee053249b7302c74
Fiona Seifert <fs158@hdm-stuttgart.de>
2024-06-18 22:09:17 +0200

    Add starRating Spec and small changes

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  // Configuration for end-to-end tests
   e2e: {
+    specPattern: "cypress/e2e", 
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 
+  // Configuration for component tests
   component: {
     devServer: {
       framework: "react",
