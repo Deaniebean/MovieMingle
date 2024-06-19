26c0a024f40ac5e3ac7e2bc4795128680955d646
India Heath <ih047@hdm-stuttgart.de>
2024-06-19 20:57:06 +0200

    mobile buttons centered and temp nav removed.

--- frontend/src/components/Winner.tsx
+++ frontend/src/components/Winner.tsx
@@ -9,7 +9,6 @@ import axios from 'axios';
 
 import './Winner.css';
 
-
 const Winner: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -25,8 +24,6 @@ const Winner: React.FC = () => {
       return genre ? genre.name : '';
     });
   }
-
-
   const getYouTubeEmbedUrl = (key: string) => {
     return `https://www.youtube.com/embed/${key}`;
   };
@@ -34,21 +31,20 @@ const Winner: React.FC = () => {
   const openTrailer = () => {
     if (movie.videos && movie.videos[0]) {
       setModalOpen(true);
-    } 
-    else {
+    } else {
       console.error('No trailer available');
     }
   };
-
 
   async function addToWatchList() {
     // const movie = movies[index];
     const poster_path = movie.poster_path
       ? 'https://image.tmdb.org/t/p/original' + movie.poster_path
       : 'default poster';
-    const trailer = movie.videos && movie.videos[0]
-      ? 'www.youtube.com/watch?v=' + movie.videos[0]?.key
-      : null;
+    const trailer =
+      movie.videos && movie.videos[0]
+        ? 'www.youtube.com/watch?v=' + movie.videos[0]?.key
+        : null;
     const date = new Date(Date.now()).toISOString().slice(0, 10);
     const rating = 0;
 
@@ -97,10 +93,11 @@ const Winner: React.FC = () => {
 
   return (
     <div>
-      <h1 className="p-6 border">nav</h1>
-      <div className="px-14 mt-14 grid justify-items-center md:container mx-auto">
+      <div className="px-14 mt-6 grid justify-items-center md:container mx-auto">
         <p className="mb-1 md:text-xl">The winner is...</p>
-        <p className="text-3xl md:text-6xl font-bold mb-4 text-center">{movie.original_title}</p>
+        <p className="text-3xl md:text-6xl font-bold mb-4 text-center">
+          {movie.original_title}
+        </p>
 
         <div className="md:flex md:mt-10">
           {movie.poster_path ? (
@@ -111,59 +108,61 @@ const Winner: React.FC = () => {
           ) : (
             <img src="path/to/default/image.jpg" />
           )}
-          <div className='ps-10'>
-              <p className="hidden md:block md:text-lg">{movie.overview}</p>
-              {movie.videos && movie.videos[0] && (
-                <>
-                   <button
-                className="movie-detail-trailer-button"
-                type="submit"
-                onClick={openTrailer}
-              > Open Trailer
-              </button>
-
-             {modalOpen && (
-                <div className="modal">
-                  <div className="modal-content">
-                    <button
-                      className="close-button " 
-                      onClick={() => setModalOpen(false)}
-                    >
-                      Close Trailer
-                    </button>
-                    <iframe
-                      width="560"
-                      height="315"
-                      src={getYouTubeEmbedUrl(movie.videos[0]?.key)}
-                      title="YouTube video player"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen
-                    ></iframe>
-                  </div>
+          <div className="pt-5 md:ps-10">
+            <p className="hidden md:block md:text-lg">{movie.overview}</p>
+            {movie.videos && movie.videos[0] && (
+              <>
+                <div className="">
+                  <button
+                    className="movie-detail-trailer-button mx-auto md:ms-0"
+                    type="submit"
+                    onClick={openTrailer}
+                  >
+                    {' '}
+                    Open Trailer
+                  </button>
+                  <button
+                    className="movie-detail-addtolist mx-auto md:ms-0"
+                    type="submit"
+                    onClick={addToWatchList}
+                  >
+                    Add to watchlist
+                  </button>
+                  <p
+                    className="movie-detail-select mx-auto md:ms-0"
+                    onClick={() => navigate('/select')}
+                  >
+                    Go to select filter
+                  </p>
                 </div>
-              )}
+
+                {modalOpen && (
+                  <div className="modal">
+                    <div className="modal-content">
+                      <button
+                        className="close-button "
+                        onClick={() => setModalOpen(false)}
+                      >
+                        Close Trailer
+                      </button>
+                      <iframe
+                        width="560"
+                        height="315"
+                        src={getYouTubeEmbedUrl(movie.videos[0]?.key)}
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                      ></iframe>
+                    </div>
+                  </div>
+                )}
               </>
             )}
-
-              <button
-                className="movie-detail-addtolist"
-                type="submit"
-                onClick={addToWatchList}
-              >
-                Add to watchlist
-              </button>
-              <p
-                className="movie-detail-select"
-                onClick={() => navigate('/select')}
-              >
-                Go to select filter
-              </p>
           </div>
         </div>
       </div>
     </div>
   );
-}; 
+};
 
 export default Winner;
-              
