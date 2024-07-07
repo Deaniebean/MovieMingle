a8303058824189384c8438b50ff8dc0909f917c3
ah247 <ah247@hdm-stuttgart.de>
2024-07-07 13:14:37 +0200

    styled not found page

--- frontend/src/components/NotFoundPage.tsx
+++ frontend/src/components/NotFoundPage.tsx
@@ -1,3 +1,15 @@
+import { Link } from 'react-router-dom';
+import './NotFoundPage.css';
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>;
-  }
+  return (
+    <div className="not-found-wrapper">
+      <p className="not-found-text1">Error</p>
+      <p className="not-found-text2">404</p>
+      <p className="not-found-text3">Sorry, the page you were looking for doesn't exist.</p>
+      <Link className="return-button" to="/home">
+        Return to Landingpage
+      </Link>
+    </div>
+  );
+}
