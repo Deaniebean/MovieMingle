03f7c6c06c50121f565bf0558961ff9cc1c26a11
India Heath <ih047@hdm-stuttgart.de>
2024-07-07 16:35:32 +0200

    hover state added

--- frontend/src/components/innerComponents/Button.tsx
+++ frontend/src/components/innerComponents/Button.tsx
@@ -14,7 +14,7 @@ const defaultProps: Partial<ButtonProps> = {
 const Button: React.FC<ButtonProps> = (props) => {
   // You can use props.prop1 and props.prop2 here
   return (
-    <button className='bg-secondary px-20 md:px-40 py-2 md:py-5 text-primary rounded-large text-xl md:text-2xl font-bold'>
+    <button className='w-full border-2 border-secondary px-20 md:px-40 py-3 md:py-5 text-white rounded-large text-xl md:text-2xl font-bold md:hover:bg-white md:hover:text-primary transition-all duration-300'>
       {props.text}
     </button>
   );
